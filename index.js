8a96882cc4bb1a1b0fc18e7bbe97cc18c0157610
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-12 23:11:04 -0300

    Rollback

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const errors = require('./src/utils/errors');
 const errorHandlerMiddleware = require('./src/middleware/errorHandlerMiddleware');
-const unhandledErrorMiddleware = require('./src/middleware/unhandledErrorMiddleware');
 
 module.exports = {
 	errors,
 	errorHandlerMiddleware,
-	unhandledErrorMiddleware,
 };
--- src/middleware/unhandledErrorMiddleware.js
+++
@@ -1,9 +0,0 @@
-module.exports = function $unhandledErrorMiddleware(errors) {
-	return (req, res, next) => {
-		process.on('unhandledRejection', (err) => {
-			return next(err);
-		});
-
-		return next();
-	};
-};
