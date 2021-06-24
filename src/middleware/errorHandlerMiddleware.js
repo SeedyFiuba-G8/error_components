7f89eaaedfbc02b8176bb4eb165cd13886f8a09a
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-24 00:46:32 -0300

    [ADD] Logger]

--- src/middleware/errorHandlerMiddleware.js
+++ src/middleware/errorHandlerMiddleware.js
@@ -1,6 +1,6 @@
 const _ = require('lodash');
 
-module.exports = function $errorHandlerMiddleware() {
+module.exports = function $errorHandlerMiddleware(logger = undefined) {
 	return (err, req, res, next) => {
 		if (res.headersSent) {
 			return next(err);
@@ -8,35 +8,22 @@ module.exports = function $errorHandlerMiddleware() {
 
 		const status = err.status || 500;
 
-		// This is probably neccesary
-		//
-		// const error = _.omitBy(
-		// 	{
-		// 		status,
-		// 		name: err.name || 'Error',
-		// 		message: err.message,
-		// 		data: err.data,
-		// 		errors: err.errors,
-		// 	},
-		// 	_.isUndefined
-		// );
-
-		const response = {
-			error: {
+		const error = _.omitBy(
+			{
 				status,
 				name: err.name || 'Error',
 				message: err.message,
 				data: err.data,
 				errors: err.errors,
 			},
-		};
-
-		console.log(
-			`[errorHandlerMiddleware] Sending back error with status ${status} and response: ${JSON.stringify(
-				response
-			)}`
+			_.isUndefined
 		);
 
-		res.status(status).json(response);
+		if (logger)
+			logger.error(
+				`Request failed. Sending error: ${JSON.stringify(error)}`
+			);
+
+		res.status(status).json({ error });
 	};
 };
